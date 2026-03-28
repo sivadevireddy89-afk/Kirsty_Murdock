bcd41ebf2176dd1b4bddfb136d40e18951566193
sivadevireddy89-afk <siva.devireddy89@gmail.com>
2026-03-27 20:39:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const galleryData = [
         title: "Ride into Sunset",
         category: "scenery",
         description: "A breathtaking view of sea in golden evening light.",
-        image: "https://raw.githubusercontent.com/sivadevireddy89-afk/rightdistractions.github.io/main/Images/WhatsApp%20Image%202026-03-24%20at%2011.22.43.jpeg"
+        image: "https://raw.githubusercontent.com/sivadevireddy89-afk/Kirsty_Murdock.github.io/main/Images/240946140_646628262971697_4122034282149787550_n.jpeg"
     },
     {
         id: 2,
