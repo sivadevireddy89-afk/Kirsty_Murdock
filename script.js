e7a455dbedfa110c267c20dfd47a5f2b65dfb390
sivadevireddy89-afk <siva.devireddy89@gmail.com>
2026-03-27 20:49:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 const galleryData = [
     {
         id: 1,
-        title: "Ride into Sunset",
-        category: "scenery",
-        description: "A breathtaking view of sea in golden evening light.",
+        title: "Majestic Elephant",
+        category: "Animals",
+        description: "A majestic elephant exploring the forest.",
         image: "https://raw.githubusercontent.com/sivadevireddy89-afk/Kirsty_Murdock/main/Images/240946140_646628262971697_4122034282149787550_n.jpeg"
     },
     {
         id: 2,
         title: "City Skyline", 
-        category: "location",
+        category: "Portrait",
         description: "London - Tower Bridge capturing the essence of modern city life.",
         image: "https://raw.githubusercontent.com/sivadevireddy89-afk/rightdistractions.github.io/main/Images/WhatsApp%20Image%202026-03-24%20at%2011.22.43%20(1).jpeg"
     },
